38577f64a60ca8ef4f1a66d9920dbef38f2003c9
Gabriel Kristian <gkqsj@cin.ufpe.br>
2019-11-09 16:14:27 -0300

    Inclusão de DataBase e aplicação base

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class  People(models.Model):
+    name = models.CharField(verbose_name = 'Nome_completo', max_length = 100)
+    cpf = models.IntegerField('CPF', primary_key = True, unique = True)
+    email = models.EmailField('E-mail', null = True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+        def __str__(self):
+            return self.name
